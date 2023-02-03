bafa28ea718852ff6b472fee42940a8c6a82cba1
Dipankar Barman <dipankarbarman.viacon@gmail.com>
2023-02-03 15:21:13 +0530

    add banner sec
    Signed-off-by: Dipankar Barman <dipankarbarman.viacon@gmail.com>

--- components/parts/hero.tsx
+++ components/parts/hero.tsx
@@ -1,2 +1,12 @@
-import Link from "next/link";
+import Link from "next/link"
 
+
+export default function Banner() {
+    return (
+        <section className="h-">
+            banner sec
+
+        </section>
+
+    );
+  }
