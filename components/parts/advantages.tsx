3762b02ecb32236a4973ec5eaca871f1dccfb967
Dipankar Barman <dipankarbarman.viacon@gmail.com>
2023-02-10 15:01:45 +0530

    need to crea te  calss component
    Signed-off-by: Dipankar Barman <dipankarbarman.viacon@gmail.com>

--- components/parts/advantages.tsx
+++ components/parts/advantages.tsx
@@ -4,7 +4,14 @@ export default function Advantages() {
   return (
     <section className="advantages-sec bg-[#252525] ">
       <div className="container mx-auto">
-        <h1 className="text-[70px] text-base-100">Advantages</h1>
+        <h1 className="text-[70px] font-semibold text-base-100">Advantages</h1>
+
+        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"> 
+
+        
+
+        </section>
+
       </div>
     </section>
   );
--- components/parts/company.tsx
+++ components/parts/company.tsx
@@ -5,7 +5,18 @@ export default function Company() {
     <section className="company-sec ">
       <div className="container mx-auto">
         <h1 className="text-7xl text-base-100">Company</h1>
-        <h2 className="text-[7.5rem] text-base-100 font-extrabold">Dot.Design</h2>
+        <h2 className="text-[7.5rem] text-base-100 font-extrabold">
+          Dot.Design
+        </h2>
+
+        <section className="btn-wrapper mt-10">
+          <a href="" className="btn bg-base-100 btn-xs sm:btn-sm md:btn-md lg:btn-lg text-black outline-none border-0">
+            Contact us
+          </a>
+        </section>
+
+        
+        
       </div>
     </section>
   );
