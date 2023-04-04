e5c1b008bd4ce40bbf9c2c675d1e7e4bb399ff9e
dipankar309 <dipankarbarman309@gmail.com>
2023-04-05 00:43:25 +0530

    add some css

--- components/parts/Slider.tsx
+++ components/parts/Slider.tsx
@@ -6,7 +6,6 @@ import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import "./styles.css";
 import { Pagination } from "swiper";
 
 
