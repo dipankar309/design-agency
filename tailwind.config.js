4c90b79b8879a759eeb5b9b2c8e5215e023c4300
dipankar309 <dipankarbarman309@gmail.com>
2023-04-08 23:52:38 +0530

    add config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        xl: '1240px',
+        '2xl': '1245px',
+      },
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
