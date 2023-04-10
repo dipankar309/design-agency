7c7292049f451858aab0032ec5c080ccd4b2c3ab
Dipankar Barman <dipankarbarman.viacon@gmail.com>
2023-04-10 15:10:36 +0530

    add space
    Signed-off-by: Dipankar Barman <dipankarbarman.viacon@gmail.com>

--- components/parts/company.tsx
+++ components/parts/company.tsx
@@ -5,17 +5,19 @@ export default function Company() {
     <section className="company-sec ">
       <div className="container mx-auto">
         <div className="contact-container">
-        <h1 className="text-7xl text-[#fff] leading-normal">Company</h1>
-        <h2 className="text-[7.5rem] text-[#fff] font-extrabold leading-normal">
-          Dot.Design
-        </h2>
-
-        <section className="btn-wrapper mt-10 flex justify-center w-full ">
-          <a href="" className="btn bg-[#fff] btn-xs sm:btn-sm md:btn-md lg:btn-lg text-black outline-none border-0">
-            Contact us
-          </a>
-        </section>
           
+          <h1 className="text-7xl text-[#fff] leading-normal">Company</h1>
+
+          <h2 className="text-[7.5rem] text-[#fff] font-extrabold leading-normal">
+            Dot.Design
+          </h2>
+
+          <section className="btn-wrapper mt-10 flex justify-center w-full ">
+            <a href="" className="btn bg-[#fff] btn-xs sm:btn-sm md:btn-md lg:btn-lg text-black outline-none border-0">
+              Contact us
+            </a>
+          </section>
+
         </div>
       </div>
     </section>
