ccc2cda7b1d6750b1e2ce54d56d35b5246176788
Dipankar Barman <dipankarbarman.viacon@gmail.com>
2023-04-10 15:08:38 +0530

    fix basic company-sec
    Signed-off-by: Dipankar Barman <dipankarbarman.viacon@gmail.com>

--- components/parts/company.tsx
+++ components/parts/company.tsx
@@ -4,19 +4,19 @@ export default function Company() {
   return (
     <section className="company-sec ">
       <div className="container mx-auto">
-        <h1 className="text-7xl text-base-100">Company</h1>
-        <h2 className="text-[7.5rem] text-base-100 font-extrabold">
+        <div className="contact-container">
+        <h1 className="text-7xl text-[#fff] leading-normal">Company</h1>
+        <h2 className="text-[7.5rem] text-[#fff] font-extrabold leading-normal">
           Dot.Design
         </h2>
 
-        <section className="btn-wrapper mt-10">
-          <a href="" className="btn bg-base-100 btn-xs sm:btn-sm md:btn-md lg:btn-lg text-black outline-none border-0">
+        <section className="btn-wrapper mt-10 flex justify-center w-full ">
+          <a href="" className="btn bg-[#fff] btn-xs sm:btn-sm md:btn-md lg:btn-lg text-black outline-none border-0">
             Contact us
           </a>
         </section>
-
-        
-        
+          
+        </div>
       </div>
     </section>
   );
