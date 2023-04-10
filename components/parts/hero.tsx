6dc0f3536c0a00c012272f097d48aa37cba375ec
dipankar309 <dipankarbarman309@gmail.com>
2023-04-10 08:54:47 +0530

    add so;me fix

--- components/parts/hero.tsx
+++ components/parts/hero.tsx
@@ -5,17 +5,24 @@ export default function Banner() {
   return (
     <section className="hero-sec">
       <div className="container mx-auto">
-        <div className="flex items-center ">
-          <h1 className="hero-title w-fit text-black">
-            <span>Design</span>
-          </h1>
-          <span className="text-black">
+        <div className="contact-container">
+          <div className="flex items-center relative ">
+            <h1 className="hero-title w-fit text-black">
+              <span>Design</span>
+            </h1>
+
+            <span className="flex w-12 bottom-b-2 border-black h-1 z-10"></span>
+          </div>
+          <span className="text-black absolute flex ">
             design that can brings insights into reality.
           </span>
+          <div className="flex justify-center">
+            <h1 className="hero-title text-black">
+              <span>Agency</span>
+            </h1>
+
+          </div>
         </div>
-        <h1 className="hero-title text-black">
-          <span>Agency</span>
-        </h1>
       </div>
     </section>
   );
