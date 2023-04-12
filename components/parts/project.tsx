00beebea2bc9937926c97ea5a0c61e5126ed8682
Dipankar Barman <dipankarbarman.viacon@gmail.com>
2023-04-12 15:02:54 +0530

    add project sec
    Signed-off-by: Dipankar Barman <dipankarbarman.viacon@gmail.com>

---
+++ components/parts/project.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Project() {
+    return (
+        <section className='project'>
+            <div className='continuer mx-auto'>
+                <div className='content-container'>
+                    <h1 className='sec-title'>
+                        Our Projects
+                    </h1>
+
+                    <section className='project-card-grid'> 
+                        
+                    </section>
+                </div>
+            </div>
+        </section>
+    )
+}
