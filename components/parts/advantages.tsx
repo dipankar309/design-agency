b58aff8ed7f1e304a6ee97eb7367dd8ce0b70537
Dipankar Barman <dipankarbarman.viacon@gmail.com>
2023-04-13 15:07:06 +0530

    add card option
    Signed-off-by: Dipankar Barman <dipankarbarman.viacon@gmail.com>

--- components/parts/advantages.tsx
+++ components/parts/advantages.tsx
@@ -4,13 +4,51 @@ export default function Advantages() {
   return (
     <section className="advantages-sec bg-[#252525] ">
       <div className="container mx-auto">
-        <h1 className="sec-title text-base-100">Advantages</h1>
+        <div className="contact-container">
+          <h1 className="sec-title text-[#ffffff] relative leading-normal">Advantages</h1>
 
-        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"> 
+          <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full">
 
-        
+            <div className="bg-red-500 w-full h-36">
+              <h2 className="text-white">
+                text aria
+              </h2>
 
-        </section>
+            </div>
+
+            <div className="bg-red-500 w-full h-36">
+              <h2 className="text-white">
+                text aria
+              </h2>
+            </div>
+            <div className="bg-red-500 w-full h-36">
+              <h2 className="text-white">
+                text aria
+              </h2>
+
+            </div>
+            <div className="bg-red-500 w-full h-36">
+              <h2 className="text-white">
+                text aria
+              </h2>
+
+            </div>
+            <div className="bg-red-500 w-full h-36">
+              <h2 className="text-white">
+                text aria
+              </h2>
+
+            </div>
+            <div className="bg-red-500 w-full h-36">
+              <h2 className="text-white">
+                text aria
+              </h2>
+
+            </div>
+
+          </section>
+
+        </div>
 
       </div>
     </section>
