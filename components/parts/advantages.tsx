d331425f42d74528f7ffb27574bbf34d44151d51
dipankar309 <dipankarbarman309@gmail.com>
2023-04-14 08:55:16 +0530

    add code

--- components/parts/advantages.tsx
+++ components/parts/advantages.tsx
@@ -7,7 +7,7 @@ export default function Advantages() {
         <div className="contact-container">
           <h1 className="sec-title text-[#ffffff] relative leading-normal">Advantages</h1>
 
-          <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full">
+          <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full mt-[1.8rem] sm:mt-[2.8rem] md:mt-[3.8rem] lg:mt-[4.8rem] xl:mt-[5.875rem]">
 
             <div className="bg-red-500 w-full h-36">
               <h2 className="text-white">
