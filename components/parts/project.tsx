626163ba66ca8f071acdf757be8de9af746d2f79
Dipankar Barman <dipankarbarman.viacon@gmail.com>
2023-04-20 15:00:47 +0530

    add grid card
    Signed-off-by: Dipankar Barman <dipankarbarman.viacon@gmail.com>

--- components/parts/project.tsx
+++ components/parts/project.tsx
@@ -5,14 +5,21 @@ export default function Project() {
         <section className='project'>
             <div className='continuer mx-auto'>
                 <div className='contact-container'>
-                    <h1 className='sec-title '>
+                    <h1 className='sec-title text-black '>
                         Our Projects
                     </h1>
 
-                    <section className='project-card-wrapper'> 
-                        
+                    <section className='project-card-wrapper'>
+                        <div className='pc-card'>
+
+                        </div>
+                        <div className='pc-card'>
+
+                        </div>
+                        <div className='pc-card'>
+
+                        </div>
                     </section>
-                    
                 </div>
             </div>
         </section>
