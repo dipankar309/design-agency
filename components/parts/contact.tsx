0704cb62291b89a0e27cf24f5d207331b5666f3e
dipankar309 <dipankarbarman309@gmail.com>
2023-04-26 01:00:38 +0530

    need to add  contact form

--- components/parts/contact.tsx
+++ components/parts/contact.tsx
@@ -4,16 +4,45 @@ export default function Contact() {
   return (
     <section className="contact">
       <div className="container mx-auto">
-        <div className="content-container">
-          <h1 className="sec-title">
+        <div className="contact-container">
+          <h1 className="sec-title text-white">
             Contact
           </h1>
 
           <section className="flex">
-            <div className="">
+            <div className="w-full md:w-1/2">
+              <div className="w-full text-white">
+                <h2 className="contact-title ">
+                  Address -
+                </h2>
+                <p className="contact-info">
+                  New Town, Kolkata
+                  West Bengal
+                </p>
+      
+              </div>
+              <div className="w-full text-white">
+                <h2 className="contact-title">
+                  Address -
+                </h2>
+                <p className="contact-info">
+                  123-456-7890
+                </p>
+                <p className="contact-info">
+                  123-456-7890
+                </p>
+              </div>
 
             </div>
-            <div>
+            <div className="w-full md:w-1/2">
+
+              <form action="" className="w-[313px] h-[341px] bg-white rounded-lg py6 px-8">
+                <h2 className="text-center">
+                  Get In Touch
+                </h2>
+                
+              </form>
+
             </div>
 
           </section>
