a4a5b2058363870d1912906e16920ca2f74875b3
dipankar309 <dipankarbarman309@gmail.com>
2023-05-15 08:41:03 +0530

    add fix

--- components/parts/contact.tsx
+++ components/parts/contact.tsx
@@ -35,16 +35,16 @@ export default function Contact() {
               </div>
 
             </div>
-            
+
             <div className="w-full md:w-1/2">
-              <form action="" className="w-[313px] h-[341px] bg-white rounded-lg py-6 px-8">
+              <form action="" className="w-[313px]  bg-white rounded-lg py-6 px-8">
                 <h2 className="text-center text-black font-semibold mb-4">
                   Get In Touch
                 </h2>
-                <input type="text" placeholder="Type here" className="input input-element" />
-                <input type="text" placeholder="Type here" className="input input-element" />
-                <textarea className="textarea w-full bg-[#EEEEEE]" placeholder="Bio"></textarea>
-                <button className="btn ">Send</button>
+                <input type="text" placeholder="Name" className="input input-element" />
+                <input type="number" placeholder="Phone" className="input input-element" />
+                <textarea className="textarea w-full bg-[#EEEEEE] text-sm mb-6 min-h-[5rem]" placeholder="Bio"></textarea>
+                <button className="btn btn-sm capitalize px-6">Send</button>
               </form>
             </div>
           </section>
